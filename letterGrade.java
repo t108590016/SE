9b1ee72b945880ef727e296029712aa83f0dbaf0
Chin-Lian Chen <t108590016@ntut.org.tw>
2023-01-02 13:11:20 +0800

    7a

--- letterGrade.java
+++ letterGrade.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class letterGrade {
     public static char letterGrade(int score) {
         char grade;
@@ -14,5 +18,20 @@ public class letterGrade {
         else
             grade = 'F';
         return grade;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enetr the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            Integer score = Integer.parseInt(br.readLine());
+            char grade = letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
